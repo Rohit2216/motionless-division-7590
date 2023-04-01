1220c12bfde4b6784b0769271d2910db6cb97411
Rohit2216 <chauhanrohit716@gmail.com>
2023-04-01 12:49:24 +0530

    done

--- Backend/index.js
+++ Backend/index.js
@@ -6,12 +6,12 @@ require("dotenv").config();
 
 const cors = require("cors");
 const { productRouter } = require("./routes/product.router");
-const { authenticate } = require("./middleware/middleware.auth");
+// const { authenticate } = require("./middleware/middleware.auth");
 app.use(express.json());
 app.use(cors());
 
 app.use("/users", userRouter);
-app.use(authenticate);
+// app.use(authenticate);
 app.use("/product", productRouter);
 
 app.listen(process.env.PORT, async () => {
