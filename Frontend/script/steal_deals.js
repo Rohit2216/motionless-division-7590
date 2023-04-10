7402d62bc6ba591326db231e167cc4ccf7f62d8a
Rohit2216 <chauhanrohit716@gmail.com>
2023-04-10 17:44:43 +0530

    done

--- Frontend/script/steal_deals.js
+++ Frontend/script/steal_deals.js
@@ -18,3 +18,29 @@ function slideshow() {
     setTimeout("slideshow()", 4000);
 }
 window.onload = slideshow;
+
+
+
+
+    // Get all the links
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    // Loop through the links and add the click event listener
+    navLinks.forEach(link => {
+      link.addEventListener('click', function() {
+        // Remove the active class from all the links
+        navLinks.forEach(link => link.classList.remove('active'));
+        // Add the active class to the clicked link
+        link.classList.add('active');
+      });
+    });
+    
+    // Get the back to top button
+    const backToTopButton = document.getElementById('back');
+    
+    // Add the click event listener to the back to top button
+    backToTopButton.addEventListener('click', function() {
+      // Scroll to the top of the page
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+    
