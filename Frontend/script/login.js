7597768f5932b93f2f484a6704134e8ce9abbaaf
Rohit2216 <chauhanrohit716@gmail.com>
2023-06-14 11:12:53 +0530

    done

--- Frontend/script/login.js
+++ Frontend/script/login.js
@@ -1,38 +1,45 @@
 function myFunction() {
-    var x = document.getElementById("password");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
   }
+}
 
 
-  const form = document.querySelector("#login-form");
-  if (form) {
-    form.addEventListener("submit", async (e) => {
-      e.preventDefault();
+const form = document.querySelector("#login-form");
+if (form) {
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
 
-      const payload = {
-        email: form.elements.email.value,
-        password: form.elements.password.value
-      };
-      // console.log(payload);
+    const payload = {
+      email: form.elements.email.value,
+      password: form.elements.password.value
+    };
 
-      fetch("https://gold-elated-turtle.cyclic.app/users/login", {
+    try {
+      const response = await fetch("https://gold-elated-turtle.cyclic.app/users/login", {
         method: "POST",
         headers: {
           "Content-type": "application/json"
         },
         body: JSON.stringify(payload)
+      });
 
-      }).then(res => res.json())
-        .then(res => {
-          localStorage.setItem("token", res.token);
-          console.log(res);
-          alert(res.msg);
-          window.location.href = "steal_deals.html";
-        })
-        .catch(err => console.log(err))
-    });
-  }
+      if (!response.ok) {
+        throw new Error("Wrong Credentials");
+      }
+
+      const res = await response.json();
+      localStorage.setItem("token", res.token);
+      console.log(res);
+      alert(res.msg);
+      window.location.href = "steal_deals.html";
+    } catch (err) {
+      console.log(err);
+      alert(err.message);
+      window.location.href = "login.html";
+    }
+  });
+}
